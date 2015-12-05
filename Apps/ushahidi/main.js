b6bd57dd7f60a96c136c225124c52053e2d74aac
Pierre Moreau <dev@pmoreau.org>
2015-12-05 20:20:03 +0100

    main.js: Set point color based on status

--- Apps/ushahidi/main.js
+++ Apps/ushahidi/main.js
@@ -11,6 +11,22 @@ xhr.onreadystatechange = function() {
 }
 xhr.send(null);
 
+function getColorForStatus(data) {
+  var wasVerified = data[23];
+  var wasActionTaken = data[25];
+  var isUrgent = data[26];
+  var wasClosed = data[27];
+  if (wasClosed == 'YES')
+    return Cesium.Color.GREY;
+  else if (wasActionTaken == 'YES')
+    return Cesium.Color.GREEN;
+  else if (isUrgent == 'YES')
+    if (wasVerified == 'YES')
+      return Cesium.Color.RED;
+    else
+      return Cesium.Color.BLUE;
+}
+
 function displayData(output) {
   var viewer = new Cesium.Viewer('cesiumContainer');
   for (i = 1; i < output.length; ++i) {
@@ -18,7 +34,7 @@ function displayData(output) {
       name : output[i][2],
       position : Cesium.Cartesian3.fromDegrees(output[i][8], output[i][7]),
       point : {
-        color : Cesium.Color.RED.withAlpha(0.5),
+        color : getColorForStatus(output[i]).withAlpha(0.8),
         outline : true,
         outlineColor : Cesium.Color.BLACK,
         pixelSize: 25
