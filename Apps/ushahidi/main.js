37a5852f956edaec56663f74a17d25a581c1f8b0
Pierre Moreau <dev@pmoreau.org>
2015-12-06 01:11:43 +0100

    Add categories

--- Apps/ushahidi/main.js
+++ Apps/ushahidi/main.js
@@ -1,16 +1,3 @@
-var output = [];
-var xhr = new XMLHttpRequest();
-xhr.open( 'GET', 'quakemap.csv', true );
-xhr.onreadystatechange = function() {
-  if ( xhr.readyState === 4 && xhr.status === 200 ) {
-    var parse = require('csv-parse');
-    parse(xhr.responseText, function(err, output) {
-      displayData(output);
-    });
-  }
-}
-xhr.send(null);
-
 function getColorForStatus(data) {
   var wasVerified = data[23];
   var wasActionTaken = data[25];
@@ -37,10 +24,73 @@ function displayData(output) {
         color : getColorForStatus(output[i]).withAlpha(0.8),
         outline : true,
         outlineColor : Cesium.Color.BLACK,
-        pixelSize: 25
+        pixelSize: 10
       },
       description : '<h1><a href="' + String(output[i][i]) + '">' + String(output[i][2]) + '</a><h1>' +
-                    '<p>' + String(output[i][5].replace(/\n/g,"<br />")) + '</p>'
+                    '<p>' + String(output[i][5]).replace(/\n/g,"<br />") + '</p>'
     });
   }
 }
+
+function setupToolbar() {
+  var displayClosed = { dispClosedEnabled : true };
+  Cesium.knockout.track(displayClosed);
+  var toolbar = document.getElementById('toolbar');
+  Cesium.knockout.applyBindings(displayClosed, toolbar);
+  Cesium.knockout.getObservable(displayClosed, 'dispClosedEnabled').subscribe(function (newValue) {});
+}
+
+function extractCategories(output) {
+  var categories = new Set();
+  for (i = 1; i < output.length; ++i) {
+    var subcategories = output[i][6].split(',');
+    for (j = 0; j < subcategories.length; ++j) {
+      if (subcategories[j] != "")
+        categories.add(subcategories[j].trim());
+    }
+  }
+  return categories;
+}
+
+function togglingCategories(state) {
+  console.log(this.id + " " + state);
+}
+
+function addCheckboxesForCategories(categories) {
+  var table = document.getElementById('checkboxesTable');
+
+  for (i = 0; i < categories.length; ++i) {
+    var row = table.insertRow(table.rows.length);
+
+    var label = row.insertCell(0);
+    label.innerHTML = categories[i];
+
+    var checkbox = row.insertCell(1);
+    var element = document.createElement("input");
+    element.name = categories[i];
+    element.type = "checkbox";
+    element.checked = true;
+    element.id = "category_" + String(i);
+    element.onclick = togglingCategories;
+    checkbox.appendChild(element);
+  }
+}
+
+
+
+var output = [];
+var categories = [];
+var xhr = new XMLHttpRequest();
+xhr.open( 'GET', 'quakemap.csv', true );
+xhr.onreadystatechange = function() {
+  if ( xhr.readyState === 4 && xhr.status === 200 ) {
+    var parse = require('csv-parse');
+    parse(xhr.responseText, function(err, output) {
+      categories = extractCategories(output);
+      addCheckboxesForCategories(Array.from(categories));
+      displayData(output);
+    });
+    setupToolbar();
+  }
+}
+xhr.send(null);
